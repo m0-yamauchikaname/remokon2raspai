766689fe5384c5a2fcac9e22fa414d6c993093e1
YamaKana020205 <72684149+m0-yamauchikaname@users.noreply.github.com>
2024-07-22 16:31:44 +0900

    Add files via upload

---
+++ remokon2raspai/src/remokon2paspai.cpp
@@ -0,0 +1,169 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  remokon2paspai.cpp
+ * @brief ModuleDescription
+ *
+ */
+// </rtc-template>
+
+#include "remokon2paspai.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const remokon2paspai_spec[] =
+#else
+static const char* remokon2paspai_spec[] =
+#endif
+  {
+    "implementation_id", "remokon2paspai",
+    "type_name",         "remokon2paspai",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "AIST",
+    "category",          "Controller",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+remokon2paspai::remokon2paspai(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_remokonIn("remokon", m_remokon),
+    m_end1In("end1", m_end1),
+    m_end2In("end2", m_end2),
+    m_ras1Out("ras1", m_ras1),
+    m_ras2Out("ras2", m_ras2)
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+remokon2paspai::~remokon2paspai()
+{
+}
+
+
+
+RTC::ReturnCode_t remokon2paspai::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("remokon", m_remokonIn);
+  addInPort("end1", m_end1In);
+  addInPort("end2", m_end2In);
+  
+  // Set OutPort buffer
+  addOutPort("ras1", m_ras1Out);
+  addOutPort("ras2", m_ras2Out);
+
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t remokon2paspai::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t remokon2paspai::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspai::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t remokon2paspai::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t remokon2paspai::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t remokon2paspai::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t remokon2paspai::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspai::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspai::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspai::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspai::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+
+extern "C"
+{
+ 
+  void remokon2paspaiInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(remokon2paspai_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<remokon2paspai>,
+                             RTC::Delete<remokon2paspai>);
+  }
+  
+}
---
+++ remokon2raspai/test/src/remokon2paspaiTest.cpp
@@ -0,0 +1,173 @@
+﻿// -*- C++ -*-
+// <rtc-template block="description">
+/*!
+ * @file  remokon2paspaiTest.cpp
+ * @brief ModuleDescription (test code)
+ *
+ */
+// </rtc-template>
+
+#include "remokon2paspaiTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+#if RTM_MAJOR_VERSION >= 2
+static const char* const remokon2paspai_spec[] =
+#else
+static const char* remokon2paspai_spec[] =
+#endif
+  {
+    "implementation_id", "remokon2paspaiTest",
+    "type_name",         "remokon2paspaiTest",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "AIST",
+    "category",          "Controller",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+remokon2paspaiTest::remokon2paspaiTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_remokonOut("remokon", m_remokon),
+    m_end1Out("end1", m_end1),
+    m_end2Out("end2", m_end2),
+    m_ras1In("ras1", m_ras1),
+    m_ras2In("ras2", m_ras2)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+remokon2paspaiTest::~remokon2paspaiTest()
+{
+}
+
+
+
+RTC::ReturnCode_t remokon2paspaiTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("ras1", m_ras1In);
+  addInPort("ras2", m_ras2In);
+  
+  // Set OutPort buffer
+  addOutPort("remokon", m_remokonOut);
+  addOutPort("end1", m_end1Out);
+  addOutPort("end2", m_end2Out);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t remokon2paspaiTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onStartup(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onShutdown(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+RTC::ReturnCode_t remokon2paspaiTest::onActivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t remokon2paspaiTest::onDeactivated(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t remokon2paspaiTest::onExecute(RTC::UniqueId /*ec_id*/)
+{
+  return RTC::RTC_OK;
+}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onAborting(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onError(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onReset(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onStateUpdate(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+//RTC::ReturnCode_t remokon2paspaiTest::onRateChanged(RTC::UniqueId /*ec_id*/)
+//{
+//  return RTC::RTC_OK;
+//}
+
+
+bool remokon2paspaiTest::runTest()
+{
+    return true;
+}
+
+
+extern "C"
+{
+ 
+  void remokon2paspaiTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(remokon2paspai_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<remokon2paspaiTest>,
+                             RTC::Delete<remokon2paspaiTest>);
+  }
+  
+}
